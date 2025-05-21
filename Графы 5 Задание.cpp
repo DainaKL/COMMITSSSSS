575d8d953c98869ead95b06794c9da88dddac5fa
Дарья <dasasor2006@gmail.com>
2025-05-21 04:47:14 +0400

    Основа 1 Задания

---
+++ Графы 5 Задание.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int n, m; // n число вершин, m число рёбер
+    cout << "Количество вершин и рёбер: ";
+    cin >> n >> m;
+    vector<vector<int>> Vhodrebra(n); // исходящие рёбра
+    vector<vector<int>> Vihodddd(n);  // Входящие рёбра
+    cout << "Ввод ребер(откуда куда):" << endl;
+    for (int i = 0; i < m; ++i) {
+        int from, to;
+        cin >> from >> to;
+        Vhodrebra[from].push_back(to); // Добав исходящее ребро
+        Vihodddd[to].push_back(from);  // Добав входящее ребро
+    }
+    return 0;
+}
